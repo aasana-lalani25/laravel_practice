5a9ac8da2ae8c676a3911677e617b856b92caf3e
aasana-lalani25 <aasanalalani25@gmail.com>
2025-01-29 08:29:40 +0530

    Add files via upload

--- views/checkout.blade.php
+++ views/checkout.blade.php
@@ -49,8 +49,10 @@
         <label for="quantity"> Quantity: </label>
         <select id="quantity" name="quantity" required>
             <option value=""> select quantity</option>
-            <option value="gujarat"> quantity</option>
-            <option value="maharastra"> quantity</option>
+            <option value="1"> 1</option>
+            <option value="2"> 2</option>
+            <option value="3"> 3</option>
+            <option value="4"> 4</option>
         </select>
         <br>
 
