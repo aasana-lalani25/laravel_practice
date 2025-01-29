d8f5a6f5eb9bca35cc16961aa75a709cb9cb32cb
aasana-lalani25 <aasanalalani25@gmail.com>
2025-01-29 08:20:09 +0530

    Add files via upload

--- Controllers/storedemo.php
+++ Controllers/storedemo.php
@@ -19,4 +19,20 @@ class storedemo extends Controller
     public function checkout(){
         return view('checkout');
     }
+
+
+    public function submitForm(Request $request)
+    {
+        $fname=$request->input('fname');
+        $lname=$request->input('lname');
+        $email=$request->input('email');
+        $number=$request->input('number');
+        $address=$request->input('address');
+        $city=$request->input('city');
+        $state=$request->input('state');
+        $price=$request->input('price');
+        $quantity=$request->input('quantity');
+        //return redirect('/contact')->with('success','Message sent successfully!');
+        return view ('checkoutsummary', compact('fname','lname','email','number','address','city','state','price','quantity'));
+    }
 }
